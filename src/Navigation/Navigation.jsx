82a8bfabe2789993c7389a90dfe8f4e483df8600
Allie Robinson <allierob93@gmail.com>
2023-01-16 22:19:09 -0600

    :lipstick: added home icon

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -1,5 +1,6 @@
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import HomeIcon from "@mui/icons-material/Home";
 import MenuIcon from "@mui/icons-material/Menu";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -8,6 +9,7 @@ import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
+
 export const Navigation = () => {
   const isUserLoggedIn = true;
   return (
@@ -28,14 +30,25 @@ export const Navigation = () => {
           </Typography>
           {!isUserLoggedIn && <Button color="inherit">Login</Button>}
           {isUserLoggedIn && (
-            <IconButton
-              color="inherit"
-              size="large"
-              edge="end"
-              aria-label="account"
-            >
-              <AccountBoxIcon />
-            </IconButton>
+            <>
+              <IconButton
+                color="inherit"
+                size="large"
+                edge="end"
+                aria-label="account"
+              >
+                <HomeIcon />
+              </IconButton>
+
+              <IconButton
+                color="inherit"
+                size="large"
+                edge="end"
+                aria-label="account"
+              >
+                <AccountBoxIcon />
+              </IconButton>
+            </>
           )}
         </Toolbar>
       </AppBar>
