802e08618eda6ca2046d69b28227e48c30ddebcd
Allie Robinson <allierob93@gmail.com>
2023-01-18 22:20:03 -0600

    :art: lil input update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import { Typography } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import { Button, TextField } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import { Box } from "@mui/system";
+import { useState } from "react";
 
 import { Navigation } from "./Navigation";
 
 function App() {
   const theme = useTheme();
+  const [listOfAccomplishments, setListOfAccomplishments] = useState([]);
+  const [newAccomplishment, setNewAccomplishment] = useState("");
+  console.log("newAccomplishment", newAccomplishment);
+  console.log("listOfAccomplishments", listOfAccomplishments);
+
   return (
     <div>
       <Navigation />
-      <Typography
-        variant="h1"
+      <Box
         sx={{
-          color: theme.palette.secondary.main,
-          textAlign: "center",
-          fontWeight: "500",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 2,
         }}
       >
-        Hi mom!!
-      </Typography>
+        <TextField
+          variant="outlined"
+          placeholder="Type accomplishment here..."
+          value={newAccomplishment}
+          sx={{ minWidth: "275px" }}
+          onChange={(e) => {
+            setNewAccomplishment(e.target.value);
+          }}
+        />
+        <Button
+          color="secondary"
+          size="large"
+          startIcon={<AddCircleOutlineIcon />}
+          onClick={() => {
+            if (newAccomplishment.trim().length) {
+              setListOfAccomplishments([
+                ...listOfAccomplishments,
+                newAccomplishment,
+              ]);
+              setNewAccomplishment("");
+            }
+          }}
+        >
+          Brag
+        </Button>
+        <ul>
+          {listOfAccomplishments.map((text, i) => (
+            <li key={`${i}-${text}`}>{text}</li>
+          ))}{" "}
+        </ul>
+      </Box>
     </div>
   );
 }
