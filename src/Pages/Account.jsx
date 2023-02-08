28009b7509c8f06b8b7af126f1260e4449ec1781
Allie Robinson <allierob93@gmail.com>
2023-02-07 23:01:36 -0600

    :sparkles: lil filtering doodad

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -1,9 +1,38 @@
-import { Typography } from "@mui/material";
+import { Checkbox, FormControlLabel, FormGroup, Stack, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useState } from "react";
+import { useLocation, useSearchParams } from "react-router-dom";
 
 import { headerFontSize } from "../constants";
 
+const dummyAccomplishments = { books: 5, meetings: 23, courses: 1 };
+
 export const Account = () => {
+  const { search } = useLocation();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [books, setBooks] = useState(searchParams.has("books"));
+  const [meetings, setMeetings] = useState(searchParams.has("meetings"));
+  const [courses, setCourses] = useState(searchParams.has("courses"));
+
+  const onInsertOrDeleteQueryParam = (queryParam, isBoxChecked, setter) => {
+    const urlSearchParams = new URLSearchParams(search);
+
+    if (isBoxChecked) {
+      setter(true);
+      const currentParams = Object.fromEntries(urlSearchParams.entries());
+      setSearchParams({ ...currentParams, [queryParam]: dummyAccomplishments[queryParam] });
+    }
+
+    if (!isBoxChecked) {
+      setter(false);
+      urlSearchParams.delete(queryParam);
+      const currentParams = Object.fromEntries(urlSearchParams.entries());
+      setSearchParams(currentParams);
+    }
+  };
+
   return (
     <Box sx={{ marginBottom: 3 }}>
       <Box
@@ -18,6 +47,42 @@ export const Account = () => {
         <Typography variant="h6" sx={{ fontSize: headerFontSize }}>
           Account Settings
         </Typography>
+        <Box sx={{ display: "flex" }}>
+          <Stack spacing={1} direction="row">
+            <FormGroup>
+              <FormControlLabel
+                onChange={(e) => onInsertOrDeleteQueryParam("books", e.target.checked, setBooks)}
+                key="books"
+                control={<Checkbox />}
+                label="Books"
+                checked={books}
+              />
+              <FormControlLabel
+                onChange={(e) =>
+                  onInsertOrDeleteQueryParam("meetings", e.target.checked, setMeetings)
+                }
+                key="meetings"
+                control={<Checkbox />}
+                label="Meetings"
+                checked={meetings}
+              />
+              <FormControlLabel
+                onChange={(e) =>
+                  onInsertOrDeleteQueryParam("courses", e.target.checked, setCourses)
+                }
+                key="courses"
+                control={<Checkbox />}
+                label="Courses"
+                checked={courses}
+              />
+            </FormGroup>
+          </Stack>
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+          {(searchParams.get("books") && Number(searchParams.get("books"))) +
+            (searchParams.get("meetings") && Number(searchParams.get("meetings"))) +
+            (searchParams.get("courses") && Number(searchParams.get("courses")))}
+        </Box>
       </Box>
     </Box>
   );
