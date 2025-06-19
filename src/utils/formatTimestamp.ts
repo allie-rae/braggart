bb6ec7b82d5f35d33a1d646f4d14bf6474796523
Allie Robinson <allierob93@gmail.com>
2025-06-19 16:12:46 -0700

    :recycle: Strict TypeScript: formatTimestamp.ts

--- src/utils/formatTimestamp.ts
+++ src/utils/formatTimestamp.ts
@@ -1,9 +1,9 @@
 import { format } from "date-fns";
 
-export const formatTimestampDayMonthHourMinute = (timestamp) => {
+export const formatTimestampDayMonthHourMinute = (timestamp: number) => {
   return format(new Date(timestamp), "E MMM dd @ h:mm a");
 };
 
-export const formatTimestampDayMonth = (timestamp) => {
+export const formatTimestampDayMonth = (timestamp: number) => {
   return format(new Date(timestamp), "E M/d");
 };
