6bedc9f578bda759aeb16af7f2b5cc9c30a14afd
Allie Robinson <allierob93@gmail.com>
2025-06-19 17:21:00 -0700

    :recycle: Strict TypeScript: index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import { routes } from "./routes";
 const router = createBrowserRouter(routes);
 
 const customTheme = createTheme(themeOptions);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <ThemeProvider theme={customTheme}>
     <RouterProvider router={router} />
