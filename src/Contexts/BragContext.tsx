b6446e42938cdfd40d0c0b26f83c0a242fafc25f
Allie Robinson <allierob93@gmail.com>
2025-07-13 21:07:55 -0700

    :clown_face: Added mock timeline data

--- src/Contexts/BragContext.tsx
+++ src/Contexts/BragContext.tsx
@@ -12,79 +12,8 @@ type BragContextType = [Brag[], (brags: Brag[]) => void];
 
 export const BragContext = createContext<BragContextType>([[], () => {}]);
 
-// keeping this for dev purposes
-// eslint-disable-next-line
+// this will allow users who do not want to interact with the app see the timeline
 const dummyTimelineData = [
-  {
-    headline: "Read Clean Code by Robert C Martin",
-    body: "Some lessons learned: 1. It is unprofessional to write messy code 2. Always leave code cleaner than you found it 3. Write variable names that reveal intention (like 'accounts' instead of 'list') 4. Clarity is king.",
-    categories: ["Productivity", "Meetings", "Upskilling"],
-    timestamp: 1675290188554,
-    id: 13,
-  },
-  {
-    headline: "Completed dependecy extraction",
-    body: `The team decided to use its own implementation instead of depending on an external library. Today, I completed the extraction of the old library.`,
-    categories: ["Upskilling"],
-    timestamp: 1674880188554,
-    id: 12,
-  },
-  {
-    headline: "Completed dependecy extraction",
-    body: `The team decided to use its own implementation instead of depending on an external library. Today, I completed the extraction of the old library.`,
-    categories: ["Meetings", "Upskilling"],
-    timestamp: 1674470188554,
-    id: 11,
-  },
-  {
-    headline: "Completed dependecy extraction",
-    body: `The team decided to use its own implementation instead of depending on an external library. Today, I completed the extraction of the old library.`,
-    categories: ["Productivity", "Meetings"],
-    timestamp: 1674400188554,
-    id: 10,
-  },
-  {
-    headline: "Completed dependecy extraction",
-    body: `The team decided to use its own implementation instead of depending on an external library. Today, I completed the extraction of the old library.`,
-    categories: ["Productivity", "Teamwork", "Upskilling"],
-    timestamp: 1674380188554,
-    id: 9,
-  },
-  {
-    headline: "Completed dependecy extraction",
-    body: `The team decided to use its own implementation instead of depending on an external library. Today, I completed the extraction of the old library.`,
-    categories: ["Teamwork"],
-    timestamp: 1674340188554,
-    id: 8,
-  },
-  {
-    headline: "Completed dependecy extraction",
-    body: `The team decided to use its own implementation instead of depending on an external library. Today, I completed the extraction of the old library.`,
-    categories: ["Productivity", "Meetings"],
-    timestamp: 1674300188554,
-    id: 7,
-  },
-  {
-    headline: "Completed dependecy extraction",
-    body: `The team decided to use its own implementation instead of depending on an external library. Today, I completed the extraction of the old library.`,
-    categories: ["Meetings"],
-    timestamp: 1674260188554,
-    id: 6,
-  },
-  {
-    headline: "Completed dependecy extraction",
-    body: `The team decided to use its own implementation instead of depending on an external library. Today, I completed the extraction of the old library.`,
-    categories: ["Productivity", "Upskilling"],
-    timestamp: 1674240188554,
-    id: 5,
-  },
-  {
-    headline: "Completed dependecy extraction",
-    body: `The team decided to use its own implementation instead of depending on an external library. Today, I completed the extraction of the old library.`,
-    categories: ["Productivity", "Meetings"],
-    timestamp: 1674220188554,
-    id: 4,
-  },
   {
     headline: "Completed dependecy extraction",
     body: `The team decided to use its own implementation instead of depending on an external library. Today, I completed the extraction of the old library.`,
@@ -113,7 +42,8 @@ export const BragContextProvider = ({ children }: { children: React.ReactNode })
   const [brags, setBrags] = useState<Brag[]>(JSON.parse(bragTimelineData) || []);
 
   useEffect(() => {
-    localStorage.setItem("bragTimelineData", JSON.stringify(brags));
+    const timelineBrags = brags.length ? brags : dummyTimelineData;
+    localStorage.setItem("bragTimelineData", JSON.stringify(timelineBrags));
   }, [brags]);
 
   return <BragContext.Provider value={[brags, setBrags]}>{children}</BragContext.Provider>;
